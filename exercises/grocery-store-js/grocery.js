074197ec1ced6ba89099c325066b5dd01bb4cd82
brighton lysy <brightonlysy@gmail.com>
2019-03-04 13:55:58 -0700

    first commit

---
+++ exercises/grocery-store-js/grocery.js
@@ -0,0 +1,13 @@
+var shopper = {
+    firstName: "Brighton",
+    lastName: "Lysy",
+    age: 34,
+    hasdog: false,
+    shoppingList: ["banana, cat food"]
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    }
+}
+shopper.shoppingList.length = (1, 2, 3, 4, 5) {
+    
+}
